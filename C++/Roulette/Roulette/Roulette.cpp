12c32c556b232ac95b3aef61591daa3006002737
E. IGuz <75509718+Ikguzzi@users.noreply.github.com>
2022-11-08 11:51:04 +0100

    Selections Done

--- C++/Roulette/Roulette/Roulette.cpp
+++ C++/Roulette/Roulette/Roulette.cpp
@@ -1,54 +1,114 @@
-#include <iostream>
+#include <iostream>;
+#include <ctime>; 
 using namespace std;
 
-int wallet = 1000;
+int gambleAge;
+int betPrize;
+int wallet = 1000 + betPrize;
 int bet;
-int valSiffra;
+int valNumber;
+int valNumberFinal;
 
+char spinConfirm;
+
+string valType;
 string valColor;
+string valColorFinal;
 
 int main()
 {
-    cout << "Välkommen till GaZoline Casino och Bar.\n";
-    cout << "Du har 1000kr värt av chips.\n";
-    cout << "Du kan satsa 100kr, 200kr eller 500kr för en chans att vinna!\n";
+    cout << "You have to be over 18 to play this game.\n";
+    
+    cout << "How old are you?\n";
+    cin >> gambleAge;
 
-    cout << "Din sats: ";
-    cin >> bet;
-
-    if (bet == 100 || bet == 300 || bet == 500)
+    if (gambleAge < 18)                                                                             // Åldersgräns
     {
-        cout << "Din sats: " << bet << "kr" << endl;
+        cout << "You have to be over 18 to play this game.\n";
+        return 0;
     }
     else
     {
-        cout << "Du måste satsa 100kr, 300kr eller 500kr";
+        cout << "Welcome to GaZoline Energy bar and casino!\n";
+    }
+
+    cout << "You have " << wallet << " kr worth of chips.\n";                                            // Definerar regler osv.
+    cout << "You can place bets worth 100, 300 or 500 kr to win amazing prices!\n";
+
+
+
+    while (bet != 100 && bet != 300 && bet != 500)
+    {
+        cout << "Your bet: ";
+        cin >> bet;
+
+        if (bet == 100 || bet == 300 || bet == 500)                                                     // Satsning
+        {
+            cout << "Your bet: " << bet << "kr" << endl;
+        }
+        else
+        {
+            cout << "You have to bet 100, 300 or 500 kr to play.\n";
+        }
     }
     
-    cout << "Vill du satsa på ett nummer eller en färg?\n";
-    cout << "Du kan satsa på ett nummer mellan 1 och 36 eller svart eller röd färg\n";
+    cout << "Do you want ot bet on a color or a number?\n";
+    cin >> valType;
 
-    cout << "Ditt val (siffra): ";
-    cin >> valSiffra;
-    cout << "Ditt val (färg): ";
-    cin >> valColor; 
+    if (valType == "Color" || valType == "color" || valType == "COLOR")                             // val mellan färg eller nummer
+    {
+        while (valColor != "Black" || valColor != "black" || valColor != "BLACK" || valColor != "Red" || valColor != "red" || valColor != "RED")
+        {
+            cout << "You're betting on a " << valType << ". Do you want to bet on red or black?\n";
+            cin >> valColor;
+        
+            if (valColor == "Black" || valColor == "black" || valColor == "BLACK" || valColor == "Red" || valColor == "red" || valColor == "RED" )
+            {
+                cout << "You've placed a bet on " << bet << " kr on " << valColorFinal << endl;
+                valColorFinal = valColor;
+            }
+            else
+            {
+                cout << "You have to choose between red or black to place a bet.\n";
+            }
+        }
+    }
+    else if (valType == "Number" || valType == "number" || valType == "Number")
+    {
+        while (valNumber <= 1 || valNumber >= 36)
+        {
+            cout << "Place a bet on a number between 1-36.\n";
+            cin >> valNumber;
 
-    if (valSiffra >= 1 && valSiffra <= 36)
+            if (valNumber >= 1 && valNumber <= 36)
+            {
+                valNumberFinal = (valNumber);
+                cout << "You've placed a bet on " << bet << " kr on number " << valNumberFinal << endl;
+            }
+            else
+            {
+                cout << "This number does not exist on the roulette wheel.\n";
+            }
+        }
+    }
+    else 
     {
-        cout << "Du satsar på>>: " << valSiffra;
+        return 0;
+    }
 
-    }
-    else if (valColor == "svart" || valColor == "rött" || valColor == "röd" || valColor == "Svart" || valColor == "Rött" || valColor == "Röd")
+    while (spinConfirm != 'Y' && spinConfirm != 'y')
     {
-        cout << "Du satsar på--: " << valColor;
-    }
-    else if (valSiffra == 0)
-    {
-        cout << "Du har fel >:(";
-    }
-    else
-    {
-        cout << "Du har fel >:)";
+        cout << "Are you ready to spin? Y/N\n";
+        cin >> spinConfirm;
+
+        if (spinConfirm == 'Y' || spinConfirm == 'y') 
+        {
+            cout << "*Spins the wheel*\n"; 
+        }
+        else
+        {
+            cout << "You need to type Y to play.\n";
+        }
     }
 }
 

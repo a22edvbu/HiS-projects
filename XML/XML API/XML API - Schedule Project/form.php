8f6518ee266004c99f9faa81742a0db64a84201e
E. IGuz <a22edvbu@student.his.se>
2023-05-09 15:17:06 +0200

    Startade med slutprojekt
    - Implementerade en form där användaren väljer linje och program.
    - Detta skickas till response.php och kommer påverka kurserna i tabellen.

--- XML/XML API/XML API - Schedule Project/form.php
+++ XML/XML API/XML API - Schedule Project/form.php
@@ -1,23 +1,56 @@
 <html>
     <body>
-        <form method='POST' action="response.php">
+        <form method='POST' action="form.php">
             <?php
             $xml = file_get_contents("https://wwwlab.webug.se/examples/XML/scheduleservice/programs/");
             $dom = new DomDocument;
             $dom->preserveWhiteSpace = FALSE;
             $dom->loadXML($xml);
-            echo "<select name='formProgram'>";
-            $programs = $dom->getElementsByTagName("program");
-            foreach ($programs as $program) {
+            echo "<select name='formDepartment'>";
+            $formDepartments = $dom->getElementsByTagName("program");
+            foreach ($formDepartments as $formDepartment) {
                 /* echo $newspaper->getAttribute("NAME"); */
-                echo "<option value='" . $program->getAttribute("id") . "'>";
-                echo $program->getAttribute("name");
+                echo "<option value='" . $formDepartment->getAttribute("department") . "'>";
+                echo $formDepartment->getAttribute("department");
                 echo "</option>";
             }
-            echo $program->getAttribute("id");   
+            echo $formDepartment->getAttribute("department");   
             echo "</select>";
+            if (isset($_POST["formDepartment"])) {
+                $formDepartment2 = $_POST['formDepartment'];
+            } else {
+                $formDepartment2 = "Empty";
+            }
             ?>
-            <button>Send</button>
+            <button>Send Department</button>
         </form>
-    </body>
-</html>
+
+            <?php
+            /* echo $formDepartment; */
+            if (isset($_POST["formDepartment"])) {
+                echo "<form method='POST' action='response.php'>";
+                $xml = file_get_contents("https://wwwlab.webug.se/examples/XML/scheduleservice/programs/?department=" . $formDepartment2);
+                $dom = new DomDocument;
+                $dom->preserveWhiteSpace = FALSE;   
+                $dom->loadXML($xml);
+                echo "<select name='formProgram'>";
+                $formPrograms = $dom->getElementsByTagName("program");
+                foreach ($formPrograms as $formProgram) {
+                    /* echo $newspaper->getAttribute("NAME"); */
+                    echo "<option value='" . $formProgram->getAttribute("id") . "'>";
+                    echo $formProgram->getAttribute("name");
+                    echo "</option>";
+                }
+                echo $formProgram->getAttribute("id");   
+                echo "</select>";   
+                echo "<button>Send Program :)</button>";
+                echo "</form>";
+            } else {
+                echo "Välj en linje.";
+            }
+
+                ?>
+
+
+</body>
+</html> 
--- XML/XML API/XML API - Schedule Project/response.php
+++ XML/XML API/XML API - Schedule Project/response.php
@@ -1,12 +1,37 @@
 <html>
     <body>
-        <?php
-            if (isset($_POST["formProgram"])) {
-                $paper = $_POST['formProgram'];
+    <?php
+            if (isset($_POST["formProgram"])){
+                $Program = $_POST['formProgram'];
             } else {
-                $paper = "Emptyy";
+                $Program = "";
             }
-            echo $paper;
+            if (isset($_POST["formDepartment"])) {
+                $formDepartment2 = $_POST['formDepartment'];
+            } else {
+                $formDepartment2 = "";
+            }
+            echo $formDepartment2;
+            echo $Program;
+
+            $xml = file_get_contents("https://wwwlab.webug.se/examples/XML/scheduleservice/courses/?department=" . $formDepartment2);
+            $dom = new DomDocument;
+            $dom->preserveWhiteSpace = FALSE;   
+            $dom->loadXML($xml);
+
+            echo "<table border=1>";
+                echo "<tr>";
+                    echo "<th>Kurs</th>";
+                    echo "<th>Sal</th>";
+                    echo "<th>Klasser</th>";
+                    echo "<th>Typ</th>";
+                    echo "<th>Tid</th>";
+                echo "</tr>";
+                $schedules = $dom->getElementsByTagName("course");
+                foreach ($schedules as $schedules){
+
+                }
+            echo "</table>";
         ?>
     </body>
-</html>
+</html> 
